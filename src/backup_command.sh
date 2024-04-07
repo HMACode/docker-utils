70e4e9201eb71f8ce18a80d3ce3d1934116cf5bd
test <test@test.test>
2024-04-07 09:08:03 +0200

    update

--- src/backup_command.sh
+++ src/backup_command.sh
@@ -15,7 +15,7 @@ fi
 
 # Default filename
 if [ -z "$output_filename" ]; then
-    output_filename="bkup-volume-$volume_name-$(date +%s).tar"
+    output_filename="bkup-volume-$volume_name-$(date +"%d-%m-%Y_%H_%M").tar"
 fi
 
 if ! [[ "$output_filename" =~ ^[a-zA-Z0-9_.-]+.tar$ ]]; then
@@ -23,4 +23,12 @@ if ! [[ "$output_filename" =~ ^[a-zA-Z0-9_.-]+.tar$ ]]; then
     exit 1
 fi
 
-docker run --rm -v $volume_name:/volume-data -v $PWD:/bkup alpine tar cf /bkup/$output_filename /volume-data
+echo "[*] Backing up volume $volume_name to file: $output_filename ..."
+docker run --rm -v $volume_name:/volume-data -v $PWD:/bkup alpine tar cf /bkup/$output_filename /volume-data &>/dev/null
+
+if [ $? -eq 0 ]; then
+    echo "[+] Backup completed successfuly"
+else
+    echo "[-] Backup failed"
+fi
+
